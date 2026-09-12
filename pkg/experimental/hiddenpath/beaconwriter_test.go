a5cb132f2afa5e686d40eeaa15b7eb70eb4110cf
GowthamNarra <gowtham.narra@st.ovgu.de>
2026-09-12 23:02:51 +0200

    typo

--- pkg/experimental/hiddenpath/beaconwriter_test.go
+++ pkg/experimental/hiddenpath/beaconwriter_test.go
@@ -373,7 +373,7 @@ func interfaceInfos(topo topology.Topology) map[uint16]ifstate.InterfaceInfo {
 			ID:           uint16(info.ID),
 			IA:           info.IA,
 			LinkType:     info.LinkType,
-			InternalAddr: netip.PrimaryInternalAddr(),
+			InternalAddr: info.PrimaryInternalAddr(),
 			RemoteID:     uint16(info.RemoteIfID),
 			MTU:          uint16(info.MTU),
 		}
